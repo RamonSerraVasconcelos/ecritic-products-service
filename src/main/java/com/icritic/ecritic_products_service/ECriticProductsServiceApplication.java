103ab7e4be79171531f0e0b26fbea5d3870f0921
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-09-24 19:59:22 -0300

    Creating eCritic products service

---
+++ src/main/java/com/icritic/ecritic_products_service/ECriticProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.icritic.ecritic_products_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECriticProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECriticProductsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icritic/ecritic_products_service/ECriticProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icritic.ecritic_products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECriticProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
