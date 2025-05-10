97ce47b5f524c02c24f214dfd77427b7c5ef3dc0
Ramon <rserravasconcelos@gmail.com>
2025-05-10 19:22:27 -0300

    Creating list items for product endpoint

--- src/main/java/com/icritic/ecritic_products_service/entrypoint/controller/ItemController.java
+++ src/main/java/com/icritic/ecritic_products_service/entrypoint/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.icritic.ecritic_products_service.entrypoint.controller;
 
 import com.icritic.ecritic_products_service.core.model.Item;
+import com.icritic.ecritic_products_service.core.model.ItemFilter;
 import com.icritic.ecritic_products_service.core.model.enums.Role;
 import com.icritic.ecritic_products_service.core.usecase.ValidateUserRoleUseCase;
 import com.icritic.ecritic_products_service.core.usecase.item.CreateItemUseCase;
@@ -10,6 +11,7 @@ import com.icritic.ecritic_products_service.core.usecase.item.UpdateItemUseCase;
 import com.icritic.ecritic_products_service.entrypoint.dto.AuthorizationTokenData;
 import com.icritic.ecritic_products_service.entrypoint.dto.ItemRequestDto;
 import com.icritic.ecritic_products_service.entrypoint.dto.ItemResponseDto;
+import com.icritic.ecritic_products_service.entrypoint.dto.Metadata;
 import com.icritic.ecritic_products_service.entrypoint.dto.PageableResponse;
 import com.icritic.ecritic_products_service.entrypoint.mapper.AuthorizationTokenDataMapper;
 import com.icritic.ecritic_products_service.entrypoint.mapper.ItemDtoMapper;
@@ -17,6 +19,8 @@ import com.icritic.ecritic_products_service.exception.ResourceViolationException
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,9 +29,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -42,9 +49,9 @@ public class ItemController {
 
     private final CreateItemUseCase createItemUseCase;
 
-    private final FindItemsUseCase findItemsUseCase;
-
     private final FindItemUseCase findItemUseCase;
+
+    private final FindItemsUseCase findItemsUseCase;
 
     private final UpdateItemUseCase updateItemUseCase;
 
@@ -79,5 +86,42 @@ public class ItemController {
         ItemResponseDto itemResponseDto = itemDtoMapper.modelToResponse(item);
 
         return ResponseEntity.status(HttpStatus.OK).body(itemResponseDto);
+    }
+
+    @GetMapping("/{productId}/items")
+    public ResponseEntity<PageableResponse<ItemResponseDto>> getItems(@RequestHeader("Authorization") String authorization,
+                                                                      @PathVariable("productId") Long productId,
+                                                                      @RequestParam(name = "active", required = false) Boolean active,
+                                                                      @RequestParam(name = "name", required = false) String name,
+                                                                      @RequestParam(name = "sku", required = false) String sku,
+                                                                      @RequestParam(name = "priceMin", required = false) BigDecimal priceMin,
+                                                                      @RequestParam(name = "priceMax", required = false) BigDecimal priceMax,
+                                                                      Pageable pageable) {
+
+        ItemFilter itemFilter = ItemFilter.builder()
+                .productId(productId)
+                .active(active)
+                .name(name)
+                .sku(sku)
+                .priceMin(priceMin)
+                .priceMax(priceMax)
+                .pageable(pageable)
+                .build();
+
+        Page<Item> items = findItemsUseCase.execute(itemFilter);
+
+        List<ItemResponseDto> itemResponseDtos = items.getContent().stream().map(itemDtoMapper::modelToResponse).toList();
+
+        PageableResponse<ItemResponseDto> pageableResponse = PageableResponse.<ItemResponseDto>builder()
+                .data(itemResponseDtos)
+                .metadata(Metadata.builder()
+                        .page(pageable.getPageNumber())
+                        .nextPage(pageable.getPageNumber() + 1)
+                        .size(pageable.getPageSize())
+                        .total(items.getTotalElements())
+                        .build())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(pageableResponse);
     }
 }
